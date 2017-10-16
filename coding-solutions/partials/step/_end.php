2aa25d136fc87b8840c56f99db50848217d72c59
Fred Parry <edwardfredparry@gmail.com>
2017-10-16 15:01:58 +0100

    Fix Badge-completed link

--- coding-solutions/partials/step/_end.php
+++ coding-solutions/partials/step/_end.php
@@ -10,4 +10,4 @@
 <p>You should now have a good idea where you can find information. You should keep up-to-date with posts to find out what others are discussing, learn new techniques from online sources (including iDEA) and read review and news site articles to find out what are the latest developments.</p>
 
 <!-- <a href="https://idea.org.uk/" class="btn">Go Back to iDEA</a> -->
-<a href="../../badge-completed.php" class="btn">Go Back to iDEA</a>
+<a href="badge-completed.php" class="btn">Go Back to iDEA</a>
